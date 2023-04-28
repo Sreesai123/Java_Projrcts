4c6bf05b13c5ab1e6c46b45b94adbfeffd77335d
SreeKanth <SreeKanth@LAPTOP-7D68NT7T>
2023-04-28 11:59:02 +0530

    Seventh Commit

---
+++ src/java_Imp_projects/Sum_of_TwoNumberWithoutThirdvariable.java
@@ -0,0 +1,20 @@
+package java_Imp_projects;
+
+import java.util.Scanner;
+
+public class Sum_of_TwoNumberWithoutThirdvariable {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the Firstnumber:");
+		int a = sc.nextInt();
+		
+		System.out.println("Enter the Secondnumber:");
+		int b = sc.nextInt();
+				
+				System.out.println("Sum of Two Number="+(a+b));
+
+	}
+
+}
---
+++ src/java_Imp_projects/Sum_of_TwoNumbers.java
@@ -0,0 +1,19 @@
+package java_Imp_projects;
+
+import java.util.Scanner;
+
+public class Sum_of_TwoNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First Number:");
+int a =sc.nextInt();
+
+System.out.println("Enter the Second Number:");
+int b = sc.nextInt();
+int sum = a+b;
+
+System.out.println("Sum of Two Nubers is ="+sum);
+	}
+
+}
