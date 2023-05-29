ad1082f20434fac7ebdf1cfd204afef54ecf7a79
SreeKanth <SreeKanth@LAPTOP-7D68NT7T>
2023-05-29 11:40:52 +0530

    NewCommits3

---
+++ src/java_Imp_projects/Ifelse_Child_Age.java
@@ -0,0 +1,14 @@
+package java_Imp_projects;
+
+public class Ifelse_Child_Age {
+
+	public static void main(String[] args) {
+		int age = 10;
+		if (age <10) {
+			System.out.println("Children Eligible for Studying In Corporate School");
+		} 
+		else {
+			System.out.println("Children is Not Eligible for Studying In Corporate School");
+		}
+	}
+}
---
+++ src/java_Imp_projects/Student_ResultsStatus.java
@@ -0,0 +1,14 @@
+package java_Imp_projects;
+
+public class Student_ResultsStatus {
+
+	public static void main(String[] args) {
+		int x = 80;
+		if (x<=80) {
+			System.out.println("Student is Pass");
+
+		}
+
+	}
+
+}
